7088d234e948ea0f356781c1c59f1560b22ee922
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-30 16:51:17 +0200

    Fixed todoColor

--- js/script.js
+++ js/script.js
@@ -1,32 +1,33 @@
-const { createApp } = Vue
+const {
+    createApp
+} = Vue
 
 createApp({
     data() {
         return {
             thingsToDo: [{
-                todo: "lavare i denti",
-                done: false
-            },
-            {
-                todo: "fare la colazione",
-                done: false
-            },
-            {
-                todo: "prendere il treno",
-                done: false
-            },
-            {
-                todo: "andare a lezione",
-                done: false
-            },
-            {
-                todo: "mangiare la colazione",
-                done: false
-            }
+                    todo: "lavare i denti",
+                    done: false
+                },
+                {
+                    todo: "fare la colazione",
+                    done: false
+                },
+                {
+                    todo: "prendere il treno",
+                    done: false
+                },
+                {
+                    todo: "andare a lezione",
+                    done: false
+                },
+                {
+                    todo: "mangiare la colazione",
+                    done: false
+                }
             ],
             text: "",
             error: false,
-            randomColor: this.randomHex()
         }
     },
     methods: {
@@ -41,6 +42,9 @@ createApp({
             }
             this.thingsToDo.push(newTodo);
             this.text = "";
+            setTimeout(this.todoColor) //Usato setTimeout per rallentare di un minimo l'esecuzione, altriimenti
+            //l'ultimo li sarebbe rimasto bianco
+
         },
         deleteList() {
             this.thingsToDo.splice(this.index, 1)
@@ -55,16 +59,20 @@ createApp({
         },
         randomHex() {
             //Si ringrazia: https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-11.php
-            let n = (Math.random() * 0xfffff * 1000000).toString(16);//Genera un numero casuale tra 0 e 1 con Math.random, moltiplicalo per il max valore esadecimale possibile e a sua volta moltiplicalo per un
+            let n = (Math.random() * 0xfffff * 1000000).toString(16); //Genera un numero casuale tra 0 e 1 con Math.random, moltiplicalo per il max valore esadecimale possibile e a sua volta moltiplicalo per un
             //numero abbastanza grande da permettere di avere un'ampia scelta di colori abbastanza unici nella propria pagina, converti poi il numero in una stringa base 16 (esadecimale)
-            return '#' + n.slice(0, 6);//Ritorna # (simbolo per scrivere un hex in css), + la stringa precedente di max lunghezza 6 (quindi taglia la stringa da indice 0 per lunghezza 6)
+            return '#' + n.slice(0, 6); //Ritorna # (simbolo per scrivere un hex in css), + la stringa precedente di max lunghezza 6 (quindi taglia la stringa da indice 0 per lunghezza 6)
 
         },
         todoColor() {
             for (li of this.$refs.todolist) {
-                li.style.background = this.randomHex()
+                if (!li.style.background) {
+
+                    li.style.background = this.randomHex()
+                }
+            }
         }
-    }
+
     },
     mounted() {
         this.todoColor();
